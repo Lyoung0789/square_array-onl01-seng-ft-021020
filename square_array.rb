2470f70132f5b7a105eb21d70e5a55af97b10d85
Lester Young <lyoung070789@gmail.com>
2020-02-11 19:54:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
-  new_array = []
+  counter=0
+  new_array=[]
   array.each do |number|
-    
-    
-    
+    new_array[counter] = number ** 2
+    counter += 1
   end
+  new_array
 end
